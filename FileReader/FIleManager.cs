2716d46aac8c16d58aa7eb54a9b5b95921c8b918
Péjó Kristóf <pejo.kristof.21i@egressy.info>
2025-04-01 10:35:24 +0200

    Add project files.

---
+++ FileReader/FIleManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileReader
+{
+    public class FileManager
+    {
+        private string fileName;
+        public FileManager(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public List<string> ReadFile()
+        {
+            List<string> all = new List<string>();
+            try
+            {
+                all = File.ReadAllLines(fileName, Encoding.UTF8).Skip(1).ToList();
+                //all.RemoveAt(all.Count-1);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return all;
+        }
+    }
+}
---
+++ FileReader/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileManager file;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        void Start()
+        {
+            file = new FileManager("data.txt");
+            List<string> all = file.ReadFile();
+            foreach (string item in all)
+            {
+                everything.Children.Add(new Label { Content = item, FontSize = 20 }); ;
+            }
+        }
+    }
+}
